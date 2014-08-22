e88d110a8cffcbadb97154841c782d60b29dffdf
habashin <habashin.feb1@gmail.com>
2014-08-22 23:41:07 +0900

    create scheduler's model and admin

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from models import Schedule, Visitor, VisitorSchedule, Court, Member
 
-# Register your models here.
+
+class VisitorScheduleInline(admin.TabularInline):
+    model = VisitorSchedule
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    inlines = [
+        VisitorScheduleInline,
+    ]
+
+
+class VisitorAdmin(admin.ModelAdmin):
+    pass
+
+
+class CourtAdmin(admin.ModelAdmin):
+    pass
+
+
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(Visitor, VisitorAdmin)
+admin.site.register(Court, CourtAdmin)
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,5 +1,66 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
+class Court(models.Model):
+    """
+    コートマスタ
+    """
+    name = models.CharField(u"コート名称", max_length=100)
+    url = models.CharField(u"コートURL", max_length=300)
+
+    def __unicode__(self):
+        return self.name
+
+
 class Schedule(models.Model):
-    event_date = models.DateField()
+    """
+    スケジュール
+    """
+    event_date = models.DateField(u"開催日")
+    start_time = models.TimeField(u"開始時間")
+    end_time = models.TimeField(u"終了時間")
+    court_id = models.OneToOneField(Court, verbose_name=u"コートID")
+    court_other = models.CharField(u"その他コート", max_length=100, null=True)
+    court_other_url = models.CharField(u"その他コート", max_length=300, null=True)
+    party_flg = models.BooleanField(u"飲み会有無")
+
+    def __unicode__(self):
+        return str(self.event_date).format("MM/DD")
+
+
+class Visitor(models.Model):
+    """
+    相手チーム
+    """
+    name = models.CharField(u"相手チーム名", max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+class VisitorSchedule(models.Model):
+    """
+    スケジュール相手チーム情報
+    """
+    schedule_id = models.ForeignKey(Schedule, verbose_name=u"スケジュールID")
+    visitor_id = models.ForeignKey(Visitor, verbose_name=u"相手チームID")
+    number = models.IntegerField(u"参加人数")
+
+
+class Member(models.Model):
+    """
+    メンバー
+    """
+    name = models.CharField(u"名前", max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
+
+class NumberOfParticipants(models.Model):
+    """
+    自チーム参加者
+    """
+    schedule_id = models.ForeignKey(Schedule, verbose_name=u"スケジュールID")
+    member_id = models.ForeignKey(Member, verbose_name=u"メンバー")
 
