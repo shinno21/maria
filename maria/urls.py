499ed2d33a785163150b8bb3db7ec70935e38736
habashin <habashin.feb1@gmail.com>
2014-09-04 22:31:43 +0900

    heroku deploy 用

--- maria/urls.py
+++ maria/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +11,10 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^scheduler/', include('scheduler.urls')),
+)
+
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 )
