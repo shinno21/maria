bd1bebf36be78f56ee3796610e10f77b61417d6c
habashin <habashin.feb1@gmail.com>
2014-09-17 00:37:46 +0900

    template 変更 url変更

--- maria/urls.py
+++ maria/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 import settings
+import scheduler.views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'maria.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', scheduler.views.schedule_list , name='scheduler_toppage'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^scheduler/', include('scheduler.urls')),
 )
